18f1f568ff181d863ef8f8eaebf19e568e0b6029
Joey3001 <josephnero111@gmail.com>
2020-11-17 11:01:52 -0800

    Working dino API isolating a single dino name
    Co-authored-by: David Sterry <sterry.david@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,33 +2,29 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Age from './js/age.js';
 
-$('#calculator').submit(function(event) {
-  event.preventDefault();
-  let newAge = new Age(parseInt($('#number').val()));
-  newAge.mercuryYears();
-  newAge.venusYears();
-  newAge.marsYears();
-  newAge.jupiterYears();
-  newAge.remainingYears();
-  newAge.remainingMercuryYears();
-  newAge.remainingVenusYears();
-  newAge.remainingMarsYears();
-  newAge.remainingJupiterYears(); 
-  $('#explanation').fadeIn("Slow");
-  newAge.lifeExpectancyBoolean();
-  newAge.lifeExpectancyPrint('#earth');
-  console.log($('#earth').text());
-  $('#mercury').text(`Mercury Age: ${newAge.mercuryAge.toFixed()}`);
-  $('#mercuryTimeLeft').text(`Years Left:  ${newAge.remainingMercuryTime.toFixed()}`);
-  $('#mars').text(`Venus Age: ${newAge.venusAge.toFixed()}`);
-  $('#marsTimeLeft').text(`Years Left: ${newAge.remainingVenusTime.toFixed()}`);
-  $('#venus').text(`Mars Age: ${newAge.marsAge.toFixed()}`);
-  $('#venusTimeLeft').text(`Years Left: ${newAge.remainingMarsTime.toFixed()}`);
-  $('#jupiter').text(`Jupiter Age: ${newAge.jupiterAge.toFixed()}`);
-  $('#jupiterTimeLeft').text(`Years Left: ${newAge.remainingJupiterTime.toFixed()}`);
-  $('.columnbox1').fadeIn("slow");
-  $('.columnbox2').fadeIn("slow");
-  $('#departingMessage').fadeIn("Slow");
+$(document).ready(function () {
+  $('#dinoform').submit(function () {
+    event.preventDefault();
+    const paragraphs = $('#1').val();
+    $('#1').val('');
+    console.log(paragraphs);
+    let request = new XMLHttpRequest();
+    const url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${paragraphs}`;
+    console.log(url);
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+    function getElements(response) {
+      $('.displaydiv').text(response[0][0]);
+      console.log(response);
+    }
+  });
 });
